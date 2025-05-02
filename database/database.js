72ba74d37a9a1d217ba024cc10af9715650ae2be
Sanami0329 <snm.k87@gmail.com>
2025-05-02 16:18:08 +0900

    fixed

--- database/database.js
+++ database/database.js
@@ -2,25 +2,26 @@
 (() => {
 
     const $button = document.getElementById('button');
+    const $email = document.getElementById('email');
+    const $password = document.getElementById('password');
+
     $button.addEventListener('click', () => {
 
-        const save_email = document.getElementById('email').value;
+        const save_email = $email.value;
         localStorage.setItem('email_key', save_email);
         const get_email = localStorage.getItem('email_key');
-        console.log(get_email);
 
-        const save_password = document.getElementById('password').value;
+        const save_password = $password.value;
         localStorage.setItem('password_key', save_password);
         const get_password = localStorage.getItem('password_key');
-        console.log(get_password);
     });
 
     document.addEventListener('DOMContentLoaded', () => {
-            const get_email = localStorage.getItem('email_key');
-            document.getElementById('email').value = get_email;
-            
-            const get_password = localStorage.getItem('password_key');
-            document.getElementById('password').value = get_password;
+        const get_email = localStorage.getItem('email_key');
+        $email.value = get_email;
+        
+        const get_password = localStorage.getItem('password_key');
+        $password.value = get_password;
     });
 
-});
+})();
